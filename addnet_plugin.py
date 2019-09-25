83c323a4bb9ba577780f6248fd9d8a693b63eced
Michele Santucci <weirdgyn@gmail.com>
2019-09-25 14:23:38 +0200

    Add plugin icon to toolbar

--- addnet_plugin.py
+++ addnet_plugin.py
@@ -7,6 +7,7 @@
 
 import wx
 import os
+import pcbnew
 
 from pcbnew import ActionPlugin, GetBoard
 from addnet_dialog import InitAddNetDialog
@@ -17,6 +18,7 @@ class AddNetPlugin(ActionPlugin):
         self.name = "AddNet"
         self.category = "Modify PCB"
         self.description = "Add a Net to a PCB"
+        self.show_toolbar_button = True
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'addnet.png')
 
     def Run(self):
